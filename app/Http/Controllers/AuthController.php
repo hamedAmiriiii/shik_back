28375bcd68f467a968d3e978a2b858a4ae6c47f6
Moh3n <amini0348@gmail.com>
2021-09-01 16:10:37 +0430

    login logout register

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'last_name' => 'required|string',
+            'atelier_id' => 'nullable|numeric',
+            'type' => 'required|numeric|digits:1',
+            'phone' => 'required|numeric|unique:users,phone|digits:11',
+            'national_code' => 'required|string|unique:users,national_code|digits:10',
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'last_name' => $fields['last_name'],
+            'atelier_id' => $fields['atelier_id'],
+            'type' => $fields['type'],
+            'phone' => $fields['phone'],
+            'national_code' => $fields['national_code'],
+            'password' => bcrypt($fields['national_code'])
+        ]);
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function login(Request $request) {
+        $fields = $request->validate([
+            'phone' => 'required|string|digits:11',
+            'password' => 'required|string'
+        ]);
+
+        // Check email
+        $user = User::where('phone', $fields['phone'])->first();
+
+        // Check password
+        if(!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'Bad creds'
+            ], 401);
+        }
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function logout(Request $request) {
+        auth()->user()->tokens()->delete();
+
+        return [
+            'message' => 'Logged out'
+        ];
+    }
+}
